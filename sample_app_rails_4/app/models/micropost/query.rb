429223fbb1362e8e32c3bf5485f7df16e1816182
Nick Dowse <nm.dowse@gmail.com>
2016-02-19 09:19:50 +0100

    Moved rails sample app to root level

---
+++ sample_app_rails_4/app/models/micropost/query.rb
@@ -0,0 +1,10 @@
+# encoding: UTF-8
+class MicroPost
+  # Returns microposts from the users being followed by the given user.
+  def self.from_users_followed_by(user)
+    followed_user_ids = "SELECT followed_id FROM relationships
+                         WHERE follower_id = :user_id"
+    where("user_id IN (#{followed_user_ids}) OR user_id = :user_id",
+          user_id: user.id)
+  end
+end
